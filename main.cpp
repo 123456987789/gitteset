95fa554520dc217f58f30d4cf00d8493b6683508
hepengpx <pengpengx.he@intel.com>
2024-03-19 11:21:01 +0800

    std::atomic counter.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,25 @@
-/*
- * Filename: main.cpp
- * Author: LucianY
- * Brief: ExactTime类测试
- */
-
 #include <iostream>
+#include <thread>
+#include <atomic>
 
-#include "exact_time.hpp"
+std::atomic<int> atomicCounter(0);
+
+void
+incrementCounter() {
+  for (int i = 0; i < 10000; ++i) {
+    atomicCounter.fetch_add(1);
+  }
+}
 
 int
 main() {
-  Ly::ExactTime exact_time = Ly::ExactTime::currentTime();
-  std::cout << "当前时间：" << exact_time.toString() << std::endl;
+  std::thread t1(incrementCounter);
+  std::thread t2(incrementCounter);
+
+  t1.join();
+  t2.join();
+
+  std::cout << "Final Counter Value: " << atomicCounter.load() << std::endl;
+
   return 0;
 }
